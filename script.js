90665e1c36ad791ba6570f18d0c01ebc5f7bf25a
l4k5hm4n <a.sailakshman@gmail.com>
2024-12-31 03:01:51 +0530

    updated changes

--- script.js
+++ script.js
@@ -2,15 +2,22 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
+    }
   });
 }
 
-
+document.onscroll = (e) => {
+  console.log(e, window.scrollY);
+  if (window.scrollY > 100) {
+    document.querySelector(".nav_bar").classList.add("sticky");
+  } else {
+    document.querySelector(".nav_bar").classList.remove("sticky");
+  }
+};
