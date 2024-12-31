6d69f73985980d2e23b35573708b49f0cfe7d3e0
sonusasankan <sonusparsham@gmail.com>
2024-12-31 10:40:14 +0530

    Add partners slider

--- script.js
+++ script.js
@@ -13,4 +13,28 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+const partnersSwiper = new Swiper('#partnersSwiper', {
+  // Optional parameters
+  slidesPerView: 6,
+  direction: 'horizontal',
+  loop: true,
+  speed: 2000,
+  autoplay: {
+    enabled: true,
+		delay: 0,
+		pauseOnMouseEnter: true,
+		disableOnInteraction: true,
+  },
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
